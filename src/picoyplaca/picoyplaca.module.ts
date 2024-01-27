103fe683e87ed69636c173401e81eec9dd597be6
Andres Arnedo <andresarnedo2002@gmail.com>
2024-01-26 23:44:11 -0500

    Update GitHub Actions workflow and add Prisma module

--- src/picoyplaca/picoyplaca.module.ts
+++ src/picoyplaca/picoyplaca.module.ts
@@ -3,10 +3,11 @@ import { PicoyplacaService } from './picoyplaca.service';
 import { PicoyplacaHandler } from './handlers/picoyplaca.handler';
 import { HttpModule } from '@nestjs/axios';
 import { SharedModule } from 'src/shared/shared.module';
+import { PrismaModule } from 'src/shared/prisma/prisma.module';
 
 @Module({
   providers: [PicoyplacaService, PicoyplacaHandler],
-  imports: [HttpModule, SharedModule],
+  imports: [HttpModule, SharedModule, PrismaModule],
   exports: [PicoyplacaHandler],
 })
 export class PicoyplacaModule {}
